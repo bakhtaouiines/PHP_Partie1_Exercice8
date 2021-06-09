b386333a2341dad76e8e9e5e75102f92421e0f57
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 14:56:44 +0200

    correction ajout HTML

--- exercice1.8.php
+++ exercice1.8.php
@@ -1,6 +1,18 @@
 <?php
-$addition = 3 + 4;
-$multiplication = 5 * 20;
-$division = 45 / 5;
-echo $addition.', '.$multiplication.', '.$division;
+    $addition = 3 + 4;
+    $multiplication = 5 * 20;
+    $division = 45 / 5;
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1.8</title>
+</head>
+<body>
+    <h1>Exercice 8 Partie 1 : Les variables PHP</h1>
+    <p><?= $addition.', '.$multiplication.', '.$division;?></p>
+</body>
+</html>
